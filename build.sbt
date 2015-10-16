73163195016a4416b20efdfa8b9043e87fd28918
Felix Becker <git@felixbecker.name>
2015-10-16 18:37:18 +0200

    step

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 libraryDependencies += "net.sourceforge.htmlcleaner" % "htmlcleaner" % "2.15"
+
+libraryDependencies += "org.jsoup" % "jsoup" % "1.8.3"
--- src/main/scala/name/felixbecker/pmdl/rawstructure/attributes/code/OpCodeGenerator.scala
+++ src/main/scala/name/felixbecker/pmdl/rawstructure/attributes/code/OpCodeGenerator.scala
@@ -3,6 +3,7 @@ package name.felixbecker.pmdl.rawstructure.attributes.code
 import java.net.URL
 
 import org.htmlcleaner.{TagNode, HtmlCleaner}
+import org.jsoup.Jsoup
 import scala.collection.JavaConversions._
 
 /**
@@ -10,28 +11,13 @@ import scala.collection.JavaConversions._
  */
 private[code] object OpCodeGenerator extends App {
 
-    val cleaner = new HtmlCleaner
-    val props = cleaner.getProperties
-    val rootNode = cleaner.clean(new URL("https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html"))
-    for(elem <- rootNode.getElementListByAttValue("class", "section-execution", true, false)){
-      val literallayout = elem.findElementByAttValue("title", "Forms", true, false)
-      val ems = literallayout.evaluateXPath("//em").collect{case t: TagNode => t}
-      if(ems.size > 1)
-      println(s"${ems.head.getText}: ${ems.tail.map(_.getText).mkString(",")}")
-      else
-      println(s"${ems.head.getText}")
-      //elem.evaluateXPath("//em").foreach(println)
-      //literallayout
-    }
-    /*val elements = rootNode.getElementsByName("a", true)
-    for (elem <- elements) {
-      val classType = elem.getAttributeByName("class")
-      if (classType != null && classType.equalsIgnoreCase("articleTitle")) {
-        // stories might be "dirty" with text like "'", clean it up
-        val text = StringEscapeUtils.unescapeHtml4(elem.getText.toString)
-        stories += text
-      }
-    }
-    return stories.filter(storyContainsDesiredPhrase(_)).toList
-    */
+  val document = Jsoup.connect("https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html").get()
+  // class section-execution
+  val sectionExecutions = document.getElementsByClass("section-execution")
+  sectionExecutions.map { element =>
+    println(element)
+    System.exit(0);
+    println(element.select("h3 em").size())
+    println("end davon")
+  }
 }
